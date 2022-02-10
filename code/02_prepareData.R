582d9a4451336c6165a0674a2b4842ef3a5ccee6
Sz-Tim <tim.szewczyk@gmail.com>
2022-02-10 15:08:35 +0000

    renamed

---
+++ code/02_prepareData.R
@@ -0,0 +1,147 @@
+# KELPER
+# Fit regressions
+# Tim Szewczyk
+
+# This script processes the datasets and covariates used to parameterize the
+# regional model for *L. hyperborea* in the UK and fits the regressions used in
+# the simulations.
+
+
+
+
+########
+##-- set up
+
+# libraries and local functions
+pkgs <- c("raster", "lubridate", "tidyverse", "sf", "brms")
+suppressMessages(invisible(lapply(pkgs, library, character.only=T)))
+walk(dir("code", "^00.*R", full.names=T), source)
+options(mc.cores=4)
+
+# switches
+PAR_datasource <- c("MODIS", "POWER")[1]
+lmType <- c("lm", "brms")[1]
+
+# directories
+gis.dir <- "..\\..\\00_gis\\"
+data.dir <- "..\\data\\digitized\\"
+lm.dir <- paste0("data\\", lmType)
+supp.f <- "..\\data\\collab\\collab_all.xlsx"
+
+# maximum extent for covariates
+UK_bbox <- st_bbox(c(xmin=-11, xmax=2, ymin=49, ymax=61), crs=st_crs(4326))
+
+# datasets
+covars.ls <- loadCovariates(gis.dir, UK_bbox, loadFile="data\\covar_ls.rds")
+data.ls <- compileDatasets(data.dir, supp.f) %>%
+  extractCovarsToDatasets(., covars.ls, PAR_datasource)
+
+
+
+
+
+
+
+
+########
+##-- Define equations
+
+reg.forms <- list(
+  lenSt_to_wtSt.lm=formula(logWtStipe ~ logLenStipe * PAR_atDepth),
+  lenSt_to_wtFr.lm=formula(logWtFrond ~ logLenStipe + sstDay_mn),
+  lenSt_to_wtTot.lm=formula(logWtTotal ~ logLenStipe * PAR_atDepth + sstDay_mn),
+  wtSt_to_wtFr.lm=formula(logWtFrond ~ logWtStipe * propClear),
+  wtFr_to_arFr.lm=formula(logAreaFrond ~ logWtFrond * PARdepth),
+  arFr_to_wtFr.lm=formula(logWtFrond ~ logAreaFrond * PARdepth),
+  canopyHeight.lm=formula(maxStipeLen ~ sstDay_mn),
+  FAI.lm=formula(logFAI ~ logPAR + logFetch),
+  N_stage.lm=formula(logN ~ stage * PAR_atDepth * sstDay_mn * logFetch),
+  N_canopy.lm=formula(logN ~ PAR_atDepth * sstDay_mn * logFetch)
+)
+
+priors <- c(prior(normal(0, 5), class=b),
+            prior(normal(0, 5), class=Intercept),
+            prior(cauchy(0, 1), class=sigma))
+
+
+
+
+
+########
+##-- Munge datasets
+
+reg.dfs <- vector("list", length(reg.forms)) %>% setNames(names(reg.forms))
+
+reg.dfs[[names(reg.forms)[1]]] <- data.ls$lengthStipe_weightStipe %>%
+  mutate(logWtStipe=log(weightStipe), 
+         logLenStipe=log(lengthStipe))
+
+reg.dfs[[names(reg.forms)[2]]] <- data.ls$lengthStipe_weightFrond %>%
+  mutate(logLenStipe=log(lengthStipe), 
+         logWtFrond=log(weightFrond))
+
+reg.dfs[[names(reg.forms)[3]]] <- data.ls$lengthStipe_weightTotal %>%
+  mutate(logLenStipe=log(lengthStipe), 
+         logWtTotal=log(weightTotal))
+
+reg.dfs[[names(reg.forms)[4]]] <- data.ls$weightStipe_weightFrond %>% 
+  mutate(propClear=as.numeric(habitat=="clearing"),
+         logWtFrond=log(weightFrond), 
+         logWtStipe=log(weightStipe))
+
+reg.dfs[[names(reg.forms)[5]]] <- data.ls$weightFrond_areaFrond %>%
+  mutate(logWtFrond=log(weightFrond),
+         logAreaFrond=log(areaFrond/1e4)) %>%
+  rename(PARdepth=PAR_atDepth)
+
+reg.dfs[[names(reg.forms)[6]]] <- reg.dfs[[names(reg.forms)[5]]]
+
+reg.dfs[[names(reg.forms)[7]]] <- data.ls$depth_maxStipeLen
+
+reg.dfs[[names(reg.forms)[8]]] <- data.ls$depth_FAI %>%
+  mutate(logFAI=log(FAI),
+         logPAR=log(PAR_atDepth),
+         logFetch=log(fetch))
+
+reg.dfs[[names(reg.forms)[9]]] <- data.ls$lengthStipe_NperSqM %>%
+  left_join(., data.ls$lengthStipe_NperSqM %>% 
+              group_by(PAR_atDepth) %>% 
+              summarise(stipeMin=min(lengthStipe), stipeMax=max(lengthStipe),
+                        top33=(stipeMax-stipeMin)*2/3 + stipeMin), 
+            by="PAR_atDepth") %>%
+  mutate(stage=c("canopy", "subcanopy")[1 + (lengthStipe < top33)]) %>%
+  group_by(sstDay_mn, PAR_atDepth, fetch, stage) %>%
+  summarise(NperSqM=sum(NperSqM)) %>%
+  bind_rows(data.ls$depth_NperSqM %>% 
+              select(2:4, sstDay_mn, PAR_atDepth, fetch) %>%
+              rename(canopy=NperSqM, subcanopy=N_subcanopy, recruits=N_recruits) %>%
+              pivot_longer(1:3, names_to="stage", values_to="NperSqM")) %>%
+  mutate(logN=log(NperSqM+1),
+         logFetch=log(fetch))
+
+reg.dfs[[names(reg.forms)[10]]] <- reg.dfs[[names(reg.forms)[9]]] %>%
+  filter(stage=="canopy")
+
+
+
+
+
+########
+##-- Fit regressions
+
+if(lmType=="lm") {
+  reg.fit <- map2(reg.forms, reg.dfs, ~lm(.x, .y))
+} else if(lmType=="brms") {
+  reg.fit <- map2(reg.forms, reg.dfs, ~brm(.x, data=.y))
+}
+
+
+
+
+
+########
+##-- Save output
+
+saveRDS(reg.fit, paste0(lm.dir, "_fits.rds"))
+
+
