d56a4e300c7cfabe7e630090d4c8a084a03f6c7f
Sz-Tim <tim.szewczyk@gmail.com>
2022-03-01 11:11:00 +0000

    updated regressions

--- code/02_fitRegressions.R
+++ code/02_fitRegressions.R
@@ -46,64 +46,44 @@ data.ls <- compileDatasets(data.dir, supp.f) %>%
 ##-- Define equations
 
 reg.full <- list(
-  lenSt_to_wtSt.lm="logWtStipe ~ logLenStipe * PAR_atDepth * sstDay_mn * fetch + (1|location)",
-  lenSt_to_wtFr.lm="logWtFrond ~ logLenStipe * PAR_atDepth * sstDay_mn * fetch + (1|location)",
-  wtFr_to_arFr.lm="logAreaFrond ~ logWtFrond * PAR_atDepth * fetch + sstDay_mn",
-  arFr_to_wtFr.lm="logWtFrond ~ logAreaFrond * PAR_atDepth * fetch + sstDay_mn",
-  canopyHeight.lm="maxStipeLen ~ sstDay_mn * PAR_atDepth * fetch",
-  FAI.lm="FAI ~ PAR_atDepth * sstDay_mn * fetch",
-  N_canopy.lm="N ~ PAR_atDepth * sstDay_mn * fetch + (1|location)"
+  lenSt_to_wtSt.lm="logWtStipe ~ logLenStipe * PAR_atDepth * SST * fetch + (1|location)",
+  lenSt_to_wtFr.lm="logWtFrond ~ logLenStipe * PAR_atDepth * SST * fetch + (1|location)",
+  wtFr_to_arFr.lm="logAreaFrond ~ logWtFrond * PAR_atDepth * fetch + SST",
+  arFr_to_wtFr.lm="logWtFrond ~ logAreaFrond * PAR_atDepth * fetch + SST",
+  canopyHeight.lm="maxStipeLen ~ SST * PAR_atDepth * fetch",
+  FAI.lm="FAI ~ PAR_atDepth * SST * fetch",
+  N_canopy.lm=c("N ~ PAR_atDepth * SST * fetch * logSlope + (1|location)",
+                "~ PAR_atDepth * SST * fetch * logSlope + (1|location)")
 )
 
-# Selected by AIC(c) using the unthinking MuMIn::dredge() 
-# -- I think these overfit. They lead to wildly extreme predictions.
-# -- Better to base each regression off of previous analyses & suggested
-# -- relationships in the literature.
-# reg.MODIS <- list(
-#   lenSt_to_wtSt.lm="logWtStipe ~ logLenStipe * sstDay_mn + (1|location)",
-#   lenSt_to_wtFr.lm=paste("logWtFrond ~ logLenStipe + PAR_atDepth + sstDay_mn + fetch +",
-#                          "logLenStipe:PAR_atDepth + logLenStipe:fetch + (1|location)"),
-#   wtFr_to_arFr.lm=paste("logAreaFrond ~ logWtFrond + PAR_atDepth + sstDay_mn + fetch +",
-#                         "logWtFrond:PAR_atDepth + fetch:PAR_atDepth"),
-#   arFr_to_wtFr.lm=paste("logWtFrond ~ logAreaFrond + PAR_atDepth + sstDay_mn + fetch +",
-#                         "logAreaFrond:PAR_atDepth + fetch:PAR_atDepth"),
-#   canopyHeight.lm="maxStipeLen ~ sstDay_mn",
-#   FAI.lm="FAI ~ PAR_atDepth + sstDay_mn",
-#   N_canopy.lm=c("N ~ PAR_atDepth + fetch + (1|location)", 
-#                 "~ PAR_atDepth * sstDay_mn + (1|location)")
-# )
-# reg.POWER <- list(
-#   lenSt_to_wtSt.lm="logWtStipe ~ logLenStipe * sstDay_mn + (1|location)",
-#   lenSt_to_wtFr.lm=paste("logWtFrond ~ logLenStipe + PAR_atDepth + fetch +",
-#                          "logLenStipe:PAR_atDepth + logLenStipe:fetch + (1|location)"),
-#   wtFr_to_arFr.lm=paste("logAreaFrond ~ logWtFrond + PAR_atDepth + sstDay_mn + fetch +",
-#                         "logWtFrond:PAR_atDepth"),
-#   arFr_to_wtFr.lm=paste("logWtFrond ~ logAreaFrond + PAR_atDepth + sstDay_mn + fetch +",
-#                         "logAreaFrond:PAR_atDepth"),
-#   canopyHeight.lm="maxStipeLen ~ sstDay_mn",
-#   FAI.lm="FAI ~ PAR_atDepth + sstDay_mn + fetch",
-#   N_canopy.lm=c("N ~ PAR_atDepth + (1|location)", 
-#                 "~ PAR_atDepth * sstDay_mn + (1|location)")
-# )
-# if(PAR_datasource=="MODIS") reg.best <- reg.MODIS
-# if(PAR_datasource=="POWER") reg.best <- reg.POWER
-
+# Could use model selected by dredge -> AICc, but probably better to take a more
+# thoughtful approach, especially given the limited number of locations
 reg.best <- list(
-  lenSt_to_wtSt.lm="logWtStipe ~ logLenStipe * PAR_atDepth + fetch + ( 1 | location )",
-  lenSt_to_wtFr.lm="logWtFrond ~ logLenStipe + sstDay_mn + ( 1 | location )",
+  # Kain 1963: doesn't change with light conditions 
+  # Bekkby 2014, ref therein: stouter to resist waves
+  lenSt_to_wtSt.lm="logWtStipe ~ logLenStipe * fetch + ( 1 | location )",
+  # Kain 1963: wtFrond ~ -depth
+  lenSt_to_wtFr.lm="logWtFrond ~ logLenStipe * PAR_atDepth + ( 1 | location )",
+  # Kain 1977: deeper water = thinner fronds; slope ~ depth
   wtFr_to_arFr.lm="logAreaFrond ~ logWtFrond * PAR_atDepth",
+  # Kain 1977: deeper water = thinner fronds; slope ~ depth
   arFr_to_wtFr.lm="logWtFrond ~ logAreaFrond * PAR_atDepth",
-  canopyHeight.lm="maxStipeLen ~ sstDay_mn",
-  FAI.lm="FAI ~ PAR_atDepth + sstDay_mn + fetch",
-  N_canopy.lm=c("N ~ PAR_atDepth * sstDay_mn + fetch + (1|location)",
-                "~ PAR_atDepth * sstDay_mn + (1|location)")
+  # Pessarrodonna 2018: driven by temp
+  canopyHeight.lm="maxStipeLen ~ SST",
+  # SST or fetch?
+  FAI.lm="FAI ~ PAR_atDepth + fetch",
+  # Bekkby 2019: PAR (depth), temp, fetch (waves + current), slope
+  # cor(logSlope, fetch) = 0.96; use fetch (lower cor with SST)
+  N_canopy.lm=c("N ~ PAR_atDepth + SST + fetch + PAR_atDepth:fetch + PAR_atDepth:SST + (1|location)",
+                "~ PAR_atDepth + SST + fetch + PAR_atDepth:fetch + PAR_atDepth:SST + (1|location)")
+  # N_canopy.lm=c("N ~ PAR_atDepth * SST + fetch + (1|location)",
+                # "~ PAR_atDepth * SST + (1|location)")
 )
 
 priors <- c(prior(normal(0, 10), class=b),
             prior(normal(0, 10), class=Intercept),
             prior(cauchy(0, 2), class=sigma),
             prior(cauchy(0, 2), class=sd))
-# Prior is still too strong on canopyHeight
 
 
 
@@ -148,10 +128,10 @@ reg.dfs$N_canopy.lm <- data.ls$lengthStipe_NperSqM %>%
                         top33=(stipeMax-stipeMin)*2/3 + stipeMin), 
             by=c("PAR_atDepth", "location")) %>%
   mutate(stage=c("canopy", "subcanopy")[1 + (lengthStipe < top33)]) %>%
-  group_by(location, reference, habitat, sstDay_mn, PAR_atDepth, fetch, stage) %>%
+  group_by(location, reference, habitat, SST, PAR_atDepth, fetch, logSlope, stage) %>%
   summarise(NperSqM=sum(NperSqM)) %>%
   bind_rows(data.ls$depth_NperSqM %>% 
-              select(2:4, location, sstDay_mn, PAR_atDepth, fetch) %>%
+              select(2:4, location, SST, PAR_atDepth, fetch, logSlope) %>%
               rename(canopy=NperSqM, subcanopy=N_subcanopy, recruits=N_recruits) %>%
               pivot_longer(1:3, names_to="stage", values_to="NperSqM")) %>%
   mutate(N=round(NperSqM)) %>%
@@ -182,7 +162,6 @@ compareModels <- F
 
 if(compareModels) {
   reg.fit <- vector("list", length(reg.full)) %>% setNames(names(reg.full))
-  
   options(na.action="na.fail")
   library(MuMIn)
   reg.fit$lenSt_to_wtSt.lm <- lmer(reg.full$lenSt_to_wtSt.lm, dfs.scaled$lenSt_to_wtSt.lm)
@@ -191,12 +170,40 @@ if(compareModels) {
   reg.fit$arFr_to_wtFr.lm <- lm(reg.full$arFr_to_wtFr.lm, dfs.scaled$arFr_to_wtFr.lm)
   reg.fit$canopyHeight.lm <- lm(reg.full$canopyHeight.lm, dfs.scaled$canopyHeight.lm)
   reg.fit$FAI.lm <- lm(reg.full$FAI.lm, dfs.scaled$FAI.lm)
-  reg.fit$N_canopy.lm <- glmmTMB(as.formula(reg.best$N_canopy.lm[1]),
-                                 ziformula=as.formula(reg.best$N_canopy.lm[2]),
+  reg.fit$N_canopy.lm <- glmmTMB(as.formula(reg.full$N_canopy.lm[1]),
+                                 ziformula=as.formula(reg.full$N_canopy.lm[2]),
                                  data=dfs.scaled$N_canopy.lm, family=nbinom2)
   reg.compare <- map(reg.fit, dredge)
   map(reg.compare, ~t(cbind(.x[1,])))
   options(na.action="na.omit") 
+  opt.params <- map(reg.compare, ~t(cbind(.x[1,]))) %>% 
+    map(~rownames(.x)[!is.na(.x)]) %>% 
+    map(~.x[! .x %in% c("(Intercept)", "df", "logLik", "AICc", "delta", "weight")]) %>%
+    map(~paste(.x, collapse=" + "))
+  opt.params$N_canopy.lm <- c(
+    opt.params$N_canopy.lm %>% 
+      str_split(., " \\+ ") %>% unlist %>%
+      grep("cond", ., value=T) %>%
+      grep("Int", ., value=T, invert=T) %>%
+      str_remove("cond\\(") %>% str_remove("\\)") %>%
+      paste(., collapse=" + "),
+    opt.params$N_canopy.lm %>% 
+      str_split(., " \\+ ") %>% unlist %>%
+      grep("zi", ., value=T) %>%
+      grep("Int", ., value=T, invert=T) %>%
+      str_remove("zi\\(") %>% str_remove("\\)") %>%
+      paste(., collapse=" + ")
+    )
+  reg.best <- list(
+    lenSt_to_wtSt.lm=paste("logWtStipe ~", opt.params$lenSt_to_wtSt.lm, "+ (1|location)"),
+    lenSt_to_wtFr.lm=paste("logWtFrond ~", opt.params$lenSt_to_wtFr.lm, "+ (1|location)"),
+    wtFr_to_arFr.lm=paste("logAreaFrond ~", opt.params$wtFr_to_arFr.lm),
+    arFr_to_wtFr.lm=paste("logWtFrond ~", opt.params$arFr_to_wtFr.lm),
+    canopyHeight.lm=paste("maxStipeLen ~", opt.params$canopyHeight.lm),
+    FAI.lm=paste("FAI ~", opt.params$FAI.lm),
+    N_canopy.lm=c(paste("N ~", opt.params$N_canopy.lm[1]),
+                  paste("~", opt.params$N_canopy.lm[2]))
+  )
 }
 
 
@@ -215,21 +222,21 @@ if(lmType=="lm") {
                                  data=dfs.scaled$N_canopy.lm, family=nbinom2)
 } else if(lmType=="brms") {
   reg.fit$lenSt_to_wtSt.lm <- brm(reg.best$lenSt_to_wtSt.lm, dfs.scaled$lenSt_to_wtSt.lm, 
-                                  prior=priors, family=gaussian())
+                                  cores=4, prior=priors, family=gaussian())
   reg.fit$lenSt_to_wtFr.lm <- brm(reg.best$lenSt_to_wtFr.lm, dfs.scaled$lenSt_to_wtFr.lm, 
-                                  prior=priors[-4,], family=gaussian())
+                                  cores=4, prior=priors[-4,], family=gaussian())
   reg.fit$wtFr_to_arFr.lm <- brm(reg.best$wtFr_to_arFr.lm, dfs.scaled$wtFr_to_arFr.lm, 
-                                 prior=priors[-4,], family=gaussian())
+                                 cores=4, prior=priors[-4,], family=gaussian())
   reg.fit$arFr_to_wtFr.lm <- brm(reg.best$arFr_to_wtFr.lm, dfs.scaled$arFr_to_wtFr.lm, 
-                                 prior=priors[-4,], family=gaussian())
+                                 cores=4, prior=priors[-4,], family=gaussian())
   reg.fit$canopyHeight.lm <- brm(reg.best$canopyHeight.lm, dfs.scaled$canopyHeight.lm, 
-                                 prior=priors[-4,], 
+                                 cores=4, prior=priors[-4,],
                                  family=gaussian())
-  reg.fit$FAI.lm <- brm(reg.best$FAI.lm, dfs.scaled$FAI.lm, 
+  reg.fit$FAI.lm <- brm(reg.best$FAI.lm, dfs.scaled$FAI.lm, cores=4, 
                         prior=priors[-4,], family=gaussian())
   reg.fit$N_canopy.lm <- brm(bf(reg.best$N_canopy.lm[1],
                                 paste("zi", reg.best$N_canopy.lm[2])), 
-                             data=dfs.scaled$N_canopy.lm, 
+                             data=dfs.scaled$N_canopy.lm, cores=4,
                              prior=priors[-3,], family=zero_inflated_negbinomial())
 }
 
@@ -239,7 +246,13 @@ if(lmType=="lm") {
 ########
 ##-- Save output
 
-saveRDS(reg.fit, glue::glue("{lm.dir}_{PAR_datasource}_fits.rds"))
-saveRDS(dfs.scaled, glue::glue("data\\dfs_scaled_{PAR_datasource}.rds"))
-saveRDS(reg.dfs_mn_sd, glue::glue("data\\dfs_mn_sd_{PAR_datasource}.rds"))
+saveRDS(reg.fit, glue::glue("data\\fits_{gridRes}_{lmType}_{PAR_datasource}.rds"))
+saveRDS(reg.best, glue::glue("data\\opt_{gridRes}_{lmType}_{PAR_datasource}.rds"))
+saveRDS(dfs.scaled, glue::glue("data\\dfs_scaled_{gridRes}_{PAR_datasource}.rds"))
+saveRDS(reg.dfs, glue::glue("data\\dfs_{gridRes}_{PAR_datasource}.rds"))
+saveRDS(reg.dfs_mn_sd, glue::glue("data\\dfs_mn_sd_{gridRes}_{PAR_datasource}.rds"))
+
+
+
+
 
