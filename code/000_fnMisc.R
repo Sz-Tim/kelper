8245f6c14fdc7795d95b8276b7fed037e4a4b189
Sz-Tim <tim.szewczyk@gmail.com>
2022-04-06 13:26:02 +0100

    multi-lag function

--- code/000_fnMisc.R
+++ code/000_fnMisc.R
@@ -985,3 +985,34 @@ runBRTs <- function(x, pop.f, mass.f, parSets, grid.i, meta.cols, params,
 }
 
 
+
+
+
+
+
+
+#' Lag multiple variables at once
+#'
+#' From https://stackoverflow.com/questions/55814028/multiple-lags-with-dplyr
+#'
+#' @param data Dataframe
+#' @param ... Unquoted variable names to lag
+#' @param n Number of lags
+#'
+#' @return
+#' @export
+#'
+#' @examples
+multijetlag <- function(data, ..., n=10){
+  library(rlang)
+  variable <- enquos(...)
+  
+  indices <- seq_len(n)
+  combos <- crossing(indices, var =as.list(variable))
+  
+  quosures <- map2(combos$indices, combos$var,
+                   ~quo(lag(!!.y, !!.x)) ) %>% 
+    set_names(paste("lag", combos$indices, map_chr(combos$var, quo_text), sep = "_"))
+  mutate( data, !!!quosures )
+  
+}
